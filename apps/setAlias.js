301c910b6edd7deebac07982770b703db6151e01
SmallK111407 <3399280843@qq.com>
2024-08-21 19:53:38 +0800

    优化 查看别名显示

--- apps/setAlias.js
+++ apps/setAlias.js
@@ -132,6 +132,7 @@ export class abbrSet extends plugin {
     for (let i in result.aliases) {
       let num = Number(i) + 1
       msg.push(`${num}.${result.aliases[i]}`)
+      msg = msg.join('\n')
     }
     let title = `${result.name}别名，${msg.length}个`
     await this.e.reply(await common.makeForwardMsg(this.e, msg, title))
