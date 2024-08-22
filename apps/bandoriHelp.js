3e50cf93f0d787245b488deda9c5fd49c29abe1a
SmallK111407 <3399280843@qq.com>
2024-08-23 04:25:37 +0800

    合并 冗余文件

--- apps/bandoriHelp.js
+++ apps/bandoriHelp.js
@@ -1,9 +1,87 @@
 import lodash from 'lodash'
 import fs from 'fs'
-import { Data } from '../../model/components/index.js'
+import { Cfg, Version, Common, Data } from '../model/components/index.js'
 
-let Theme = {
-  async getThemeCfg (theme, exclude) {
+export class bandoriHelp extends plugin {
+  constructor() {
+    super({
+      name: '[邦邦插件]帮助',
+      dsc: '获取邦邦插件帮助',
+      event: 'message',
+      priority: 10,
+      rule: [
+        {
+          reg: "^#?(邦邦|邦多利)?(命令|帮助|菜单|help|说明|功能|指令|使用说明)$",
+          fnc: 'help'
+        },
+        {
+          reg: "^#?(邦邦|邦多利)版本$",
+          fnc: 'versionInfo'
+        }
+      ]
+    })
+  }
+
+  async help(e) {
+    if ((!/邦邦/.test(e.msg)) && (!/邦多利/.test(e.msg)) && !Cfg.get('sys.help', false)) {
+      return false
+    }
+
+    let custom = {}
+    let help = {}
+    let { diyCfg, sysCfg } = await Data.importCfg('help')
+
+    if (lodash.isArray(help.helpCfg)) {
+      custom = {
+        helpList: help.helpCfg,
+        helpCfg: {}
+      }
+    } else {
+      custom = help
+    }
+
+    let helpConfig = lodash.defaults(diyCfg.helpCfg || {}, custom.helpCfg, sysCfg.helpCfg)
+    let helpList = diyCfg.helpList || custom.helpList || sysCfg.helpList
+
+    let helpGroup = []
+
+    lodash.forEach(helpList, (group) => {
+      if (group.auth && group.auth === 'master' && !e.isMaster) {
+        return true
+      }
+
+      lodash.forEach(group.list, (help) => {
+        let icon = help.icon * 1
+        if (!icon) {
+          help.css = 'display:none'
+        } else {
+          let x = (icon - 1) % 10
+          let y = (icon - x - 1) / 10
+          help.css = `background-position:-${x * 50}px -${y * 50}px`
+        }
+      })
+
+      helpGroup.push(group)
+    })
+
+    let themeData = await this.getThemeData(diyCfg.helpCfg || {}, sysCfg.helpCfg || {})
+    return await Common.render('help/index', {
+      helpCfg: helpConfig,
+      helpGroup,
+      ...themeData,
+      element: 'default'
+    }, { e, scale: 1.6 })
+  }
+
+  async versionInfo(e) {
+    return await Common.render('help/version-info', {
+      currentVersion: Version.version,
+      changelogs: Version.changelogs,
+      elem: 'pyro'
+    }, { e, scale: 1.6 })
+  }
+
+  async getThemeCfg(theme, exclude) {
     let dirPath = './plugins/bandori-plugin/resources/help/theme/'
     let ret = []
     let names = []
@@ -31,13 +109,14 @@ let Theme = {
       bg: fs.existsSync(`${dirPath}${name}/bg.jpg`) ? `${resPath}${name}/bg.jpg` : `${resPath}default/bg.jpg`,
       style: (await Data.importModule(`resources/help/theme/${name}/config.js`)).style || {}
     }
-  },
-  async getThemeData (diyStyle, sysStyle) {
+  }
+
+  async getThemeData(diyStyle, sysStyle) {
     let helpConfig = lodash.extend({}, diyStyle, sysStyle)
     let colCount = Math.min(5, Math.max(parseInt(helpConfig?.colCount) || 3, 2))
     let colWidth = Math.min(500, Math.max(100, parseInt(helpConfig?.colWidth) || 265))
     let width = Math.min(2500, Math.max(800, colCount * colWidth + 30))
-    let theme = await Theme.getThemeCfg(helpConfig.theme, diyStyle.themeExclude || sysStyle.themeExclude)
+    let theme = await this.getThemeCfg(helpConfig.theme, diyStyle.themeExclude || sysStyle.themeExclude)
     let themeStyle = theme.style || {}
     let ret = [`
     body{background-image:url(${theme.bg});width:${width}px;}
@@ -65,4 +144,3 @@ let Theme = {
     }
   }
 }
-export default Theme
